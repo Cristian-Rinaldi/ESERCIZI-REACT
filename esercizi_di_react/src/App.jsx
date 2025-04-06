ae672236020e4452b5b48b9c7655df1b618c734a
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-06 19:01:03 +0200

    modificato app.jsx per il primo esercizio

--- esercizi_di_react/src/App.jsx
+++ esercizi_di_react/src/App.jsx
@@ -1,22 +1,7 @@
-import Card from "./Card";
-
-
 function App() {
-  const cardItem = {
-    img: "assets/react.svg",
-    titolo: "React",
-    content: "Una libreria per interfacce utente"
-  };
 
   return (
-    <div className="app-container">
       <h1>Hello World</h1>
-      <Card
-        img={cardItem.img}
-        titolo={cardItem.titolo}
-        content={cardItem.content}
-      />
-    </div>
   );
 }
 
