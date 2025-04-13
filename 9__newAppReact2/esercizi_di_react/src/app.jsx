ae691034f1eac89adfc950123cb5e03f9870123d
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-13 12:42:14 +0200

    add modifche

--- 9__newAppReact2/esercizi_di_react/src/app.jsx
+++ 9__newAppReact2/esercizi_di_react/src/app.jsx
@@ -3,7 +3,7 @@ import Messaggio from "./messaggio";
 import "./style.css";
 
 function App() {
-  // Stato per i messaggi
+
   const [messages, setMessages] = useState([
     { text: "ciao cristian", className: "" },
     { text: "ciao come stai?", className: "blu-right" },
@@ -13,39 +13,38 @@ function App() {
     { text: "ok buona giornata", className: "blu-right" },
   ]);
 
-  // Stato per il messaggio che l'utente scrive
+
   const [newMessage, setNewMessage] = useState("");
 
-  // Funzione per inviare il messaggio
+  
   const handleSubmit = (e) => {
-    e.preventDefault(); // evita il refresh della pagina
+    e.preventDefault(); 
 
-    if (newMessage.trim() === "") return; // evita di inviare messaggi vuoti
+    if (newMessage.trim() === "") return;
 
-    // Aggiunge il messaggio dell'utente
     setMessages([...messages, { text: newMessage, className: "blu-right" }]);
 
-    // Svuota il campo input
+   
     setNewMessage("");
 
-    // Dopo 1 secondo, genera una risposta casuale
+  
     setTimeout(() => {
       const randomResponses = [
         "scusa ora devo andare",
         "eh perdonami devo proprio andare",
         "sono un po occupato ora parliamo dopo?",
-        "devo veramente andare via...?",
+        "devo veramente andare via...",
         "scusami...devo andare"
       ];
-      // Genera una risposta casuale da uno degli elementi dell'array
+     
       const randomResponse = randomResponses[Math.floor(Math.random() * randomResponses.length)];
 
-      // Aggiungi la risposta al contrario (dalla parte sinistra)
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        { text: randomResponse, className: "" } // risposta con sfondo diverso
+     
+      setMessages((messaggiPrecedenti) => [
+        ...messaggiPrecedenti,
+        { text: randomResponse, className: "" } 
       ]);
-    }, 1000); // Risposta dopo 1 secondo
+    }, 2000); 
   };
 
   return (
@@ -53,18 +52,18 @@ function App() {
       <section>
         <main>
           <h1>APPLICAZIONE REACT</h1>
-
-          {/* Mostra tutti i messaggi */}
+        
+        <hr />
           {messages.map((msg, index) => (
             <Messaggio key={index} text={msg.text} className={msg.className} />
           ))}
 
-          {/* Form per inserire il nuovo messaggio */}
+      
           <form onSubmit={handleSubmit}>
             <input
               type="text"
               id="text"
-              placeholder="Digita un messaggio"
+              placeholder="  Digita un messaggio"
               value={newMessage}
               onChange={(e) => setNewMessage(e.target.value)}
             />
